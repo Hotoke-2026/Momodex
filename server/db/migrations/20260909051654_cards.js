de39c267682ee70c745108f3b40b75a9f5a46f29
Morah Lopati <lopaticumorah@gmail.com>
2026-09-09 17:18:41 +1200

    create cards migration table

--- server/db/migrations/20260909051654_cards.js
+++ server/db/migrations/20260909051654_cards.js
@@ -1,15 +1,23 @@
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+ *
+ **/
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+export async function up(knex) {
+  await knex.schema.createTable('cards', (table) => {
+    table.increments('id').primary()
+    table.string('user_id').notNullable()
+    table.string('species_id').notNullable()
+    table.string('image_url').notNullable()
+    table.string('location')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+
+    table.foreign('user_id').references('id').inTable('users')
+    table.foreign('species_id').references('id').inTable('species')
+  })
+}
+
+export async function down(knex) {
+  await knex.schema.dropTable('cards')
+}
