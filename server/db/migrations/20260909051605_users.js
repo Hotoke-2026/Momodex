a94729158e278a8f46f10f4fcfb4b1268333936c
Morah Lopati <lopaticumorah@gmail.com>
2026-09-09 17:16:41 +1200

    create users table migrations

--- server/db/migrations/20260909051605_users.js
+++ server/db/migrations/20260909051605_users.js
@@ -1,15 +1,16 @@
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+ *
+ **/
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+export async function up(knex) {
+  await knex.schema.createTable('users', (table) => {
+    table.string('id').primary()
+    table.string('name').notNullable()
+  })
+}
+
+export async function down(knex) {
+  await knex.schema.dropTable('users')
+}
