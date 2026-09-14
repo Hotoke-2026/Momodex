308ac129c579080cd49c381cee580a2eba8e880c
Morah Lopati <lopaticumorah@gmail.com>
2026-09-14 16:07:47 +1200

    add user api functions for profile page

--- client/apis/users.ts
+++ client/apis/users.ts
@@ -1,5 +1,6 @@
 // client/apis/users.ts
 import request from 'superagent'
+import { BattleStats, FavouriteSpecies } from '../../models/types'
 
 export interface User {
   id: string
@@ -11,4 +12,20 @@ const rootURL = new URL(`/api/v1`, document.baseURI)
 export async function getUserById(id: string) {
   const response = await request.get(`${rootURL}/users/${id}`)
   return response.body as User
+}
+
+export interface User {
+  id: string
+  name: string
+  avatar_url?: string | null
+}
+
+export async function getBattleStats(userId: string) {
+  const response = await request.get(`${rootURL}/users/${userId}/battle-stats`)
+  return response.body as BattleStats
+}
+
+export async function getFavouriteSpecies(userId: string) {
+  const response = await request.get(`${rootURL}/users/${userId}/favourite-species`)
+  return response.body as FavouriteSpecies | null
 }
