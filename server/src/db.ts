919950fa2124a1491fc949d37ee0cb48aef783ac
Your Levi Taylor <kinosj74@gmail.com>
2026-09-18 17:43:27 +1200

    fix(render): configure Turso as sql db

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,7 +1,15 @@
-import knex from 'knex';
-import config from '../knexfile';
+import { createClient } from '@libsql/client';
 
-const environment = process.env.NODE_ENV || 'development';
-const db = knex(config[environment]);
+const url = process.env.TURSO_DATABASE_URL;
+const authToken = process.env.TURSO_AUTH_TOKEN;
+
+if (!url) {
+  throw new Error("TURSO_DATABASE_URL environment variable is missing");
+}
+
+const db = createClient({
+  url,
+  authToken,
+});
 
 export default db;
