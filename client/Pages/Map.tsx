c7476078add9a5be876673a865b3edb7fa793840
Your Levi Taylor <kinosj74@gmail.com>
2026-09-20 04:59:58 +1200

    fix(render): fix map markers

--- client/Pages/Map.tsx
+++ client/Pages/Map.tsx
@@ -1,9 +1,17 @@
 import { useMemo, useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import { useMapObservations } from '../hooks/useMapObservations'
 import { NavBar } from '../components/NavBar'
 import { Footer } from '../components/Footer'
+
+delete (L.Icon.Default.prototype as any)._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+})
 
 const DATE_RANGES = {
   all: { label: 'All time', days: null },
