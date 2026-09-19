61d06fba9f099e5fe26c62992b68c206b65deef6
Your Levi Taylor <kinosj74@gmail.com>
2026-09-20 02:51:14 +1200

    fix(blender): update connection

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,17 +1,16 @@
-import knex from 'knex'
+import { createClient } from '@libsql/client'
+import { knex } from 'knex'
+
+const client = createClient({
+  url: process.env.DATABASE_URL || process.env.TURSO_DATABASE_URL || 'file:./dev.sqlite',
+  authToken: process.env.DATABASE_AUTH_TOKEN || process.env.TURSO_AUTH_TOKEN || '',
+})
 
 const db = knex({
   client: 'sqlite3',
-  connection: process.env.DATABASE_URL?.startsWith('libsql://') || process.env.TURSO_DATABASE_URL
-    ? {
-        filename: process.env.DATABASE_URL || process.env.TURSO_DATABASE_URL || '',
-        ...(process.env.DATABASE_AUTH_TOKEN || process.env.TURSO_AUTH_TOKEN
-          ? { authToken: process.env.DATABASE_AUTH_TOKEN || process.env.TURSO_AUTH_TOKEN }
-          : {}),
-      }
-    : {
-        filename: process.env.DATABASE_URL || './dev.sqlite',
-      },
+  connection: {
+    filename: process.env.DATABASE_URL || process.env.TURSO_DATABASE_URL || './dev.sqlite',
+  },
   useNullAsDefault: true,
 })
 
